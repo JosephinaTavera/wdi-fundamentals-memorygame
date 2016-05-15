5ca4d2dddcea1c26e9d6b784d45126ca3eed58e1
Josephina <joisewurld@gmail.com>
2016-05-15 08:54:26 -0400

    submit unworking prgm

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,18 +1,49 @@
 // created variables
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen","king", "king"];
+var cardsInPlay = [];
 
 var boardVar = document.getElementById('game-board');
 
+var isMatch = function(inPlay){
+	// test two cards match 
+	if (inPlay[0] === inPlay[1]){
+		return true
+	}
+}
+
+//checks to see if there are cards in play
+function isTwoCards() {
+
+  // add card to array of cards in play
+  // 'this' hasn't been covered in this prework, but
+  // for now, just know it gives you access to the card the user clicked on
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+
+    // clear cards in play array for next try
+    cardsInPlay = [];
+
+  }
+
+}
+
+// encounter problems when adding DOM commands.  
 
 var createBoard = function(){
-	for (var i =0; i < 4; i++){
+	for (var i =0; i < cards.length; i++){
+
 	var deck = document.createElement('div');
 	deck.className = "card";
 	boardVar.appendChild(deck);
+    boardVar.setAttribute('data-card', cards[i]);	
+	deck.addEventListener('click', isTwoCards);     
+//deck.innerHTML = '<img src="KDiamonds.png" alt="King of Diamonds" />';.
 }
 }
 
